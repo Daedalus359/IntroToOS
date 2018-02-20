3cb413dc037eee865e21b191c8647709b65de97f
Daedauls359 <kbradner112@gmail.com>
2018-02-19 20:52:20 -0500

    hw4 complete

---
+++ hw4/shared_mem.c
@@ -0,0 +1,83 @@
+//Kevin Bradner, kmb172@case.edu
+//Modified from shm_producer & shm_consumer example files
+
+#include <unistd.h>
+#include <sys/types.h>
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int main()
+{
+	const int SIZE = 4096;
+	const char *name = "OS";
+	const char *message0= "Hello";
+	const char *message1= "From ";
+	const char *message2= "Your Child!";
+
+	pid_t pid;
+
+	int shm_fd;
+	void *ptr;
+
+	/* create the shared memory segment */
+	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+	/* configure the size of the shared memory segment */
+	ftruncate(shm_fd,SIZE);
+
+	/* now map the shared memory segment in the address space of the process */
+	ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (ptr == MAP_FAILED) {
+		printf("Map failed\n");
+		return -1;
+	}
+
+	
+
+	pid = fork();
+
+	//have the parent wait for the child to write, then read
+	if(pid > 0){
+		wait(NULL);
+
+		/* now read from the shared memory region */
+		printf("%s\n",ptr);
+		
+
+
+		/* remove the shared memory segment */
+		if (shm_unlink(name) == -1) {
+			printf("Error removing %s\n",name);
+			exit(-1);
+		}
+	}
+
+	//have the child write the message
+	else{
+
+	/**
+	 * Now write to the shared memory region.
+ 	 *
+	 * Note we must increment the value of ptr after each write.
+	 */
+	sprintf(ptr,"%s",message0);
+	ptr += strlen(message0);
+	sprintf(ptr,"%s",message1);
+	ptr += strlen(message1);
+	sprintf(ptr,"%s",message2);
+	ptr += strlen(message2);
+		
+	}
+
+
+
+	return 0;
+}
--- hw4/variable_core_adder.c
+++ hw4/variable_core_adder.c
@@ -7,25 +7,45 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define N 500000000
+#define N 2000000000
 
 int main() {
+
+	int numCores;
+	int coresCreated = 1;
+
+	printf("Enter a numer of cores to use between 1 and 4: ");
+	scanf("%d", &numCores);
+
+	if(numCores < 1 || numCores > 4){
+		printf("Invlaid Choice. Terminating program.");
+		return 1;
+	}
+
 	long int i;
 	long long int x = 0;
-	pid_t pid;
+	pid_t pid = 0;
 	struct timeval start_time, stop_time, elapsed_time;  // timers
 	gettimeofday(&start_time,NULL); // Unix timer
-	pid = fork();
-	if (pid > 0) {
-		pid = fork();
-		if (pid > 0) {
+
+	//create the right numer of processes
+	while(numCores > coresCreated){
+		if(pid == 0){
 			pid = fork();
+			coresCreated++;
+		}else{
+			break;
 		}
 	}
-	for (i = 0; i < N; i++) {
+
+	for (i = 0; i < (N / numCores); i++) {
 		x = x + 1;
 	}
+
 	gettimeofday(&stop_time,NULL);
 	timersub(&stop_time, &start_time, &elapsed_time); // Unix time subtract routine
 	printf("Process %d total time was %f seconds. x = %lld.\n", getpid(), elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0, x);
+	if(pid > 0){
+		wait(NULL);
+	}
 }
