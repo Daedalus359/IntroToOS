c0c99d8b65c62419453b143fc5878cc9b193daed
kevin <kbradner1122gmail.com>
2018-04-03 20:18:19 -0400

    most of hw8 p1

--- hw8/hw7_1.c
+++ hw8/hw7_1.c
@@ -10,15 +10,32 @@
 #define N 10
 #define BUFFER_SIZE 5
 
+// Struct for condition variable
+struct condition {
+	sem_t sem; // Semaphore to be initialized to 0
+	int count; // Count of threads waiting
+};
+
+//not empty count: number of producers waiting to produce
+//not full count: number of consumers waiting to consume
+
+struct condition not_full, not_empty;
+//call cpost not empty after adding an item
+//call cpost not full 
+
+void cwait(struct condition *c); // Semaphore implementation of conditional wait
+void cpost(struct condition *c); // Semaphore implementation of conditional signal
+
 /* Global variables are shared by the thread(s) */
-sem_t mutex, full, empty;
+sem_t mutex, next; //got rid of original 2nd and 3rd, added a next
 int buffer[BUFFER_SIZE];
 
 void *child(); /* child thread */
 
 int main(int argc, char *argv[])
 {
-	if(sem_init(&mutex, 0, 1) < 0 || sem_init(&full, 0, 0) < 0 || sem_init(&empty, 0, BUFFER_SIZE) < 0) { // 0 = multithreaded
+	//init sems within the conditions
+	if(sem_init(&mutex, 0, 1) < 0 || sem_init(&next, 0, 0) < 0 || sem_init(&(not_full.sem), 0, 0) < 0 || sem_init(&(not_empty.sem), 0, 0) < 0) { // 0 = multithreaded
 		fprintf(stderr, "ERROR: could not initialize &semaphore.\n");
 		exit(0);
 	}
@@ -32,12 +49,20 @@ int main(int argc, char *argv[])
 	int data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // Must have N values
 	int i, in = 0; // Index for writing
 	for(i = 0; i < N; i++) {
-		sem_wait(&empty);
+		cwait(&not_full);//CHECK??
 		sem_wait(&mutex);
 		sleep((double) data[i] / 50); // Not required. Used for testing.
 		buffer[in] = data[i];
-		sem_post(&mutex);
-		sem_post(&full);
+		cpost(&not_empty);//CHECK?
+
+
+		//CHECK??
+		if (next_count > 0)
+			sem_post(&next);
+		else{
+			sem_post(&mutex);
+			out = (out + 1) % BUFFER_SIZE;
+		}
 		in = (in + 1) % BUFFER_SIZE;
 	}
 	// Join and print result
@@ -50,14 +75,40 @@ int main(int argc, char *argv[])
 void *child() {
 	int i, out = 0; // Index for reading
 	for(i = 0; i < N; i++) {
-		sem_wait(&full);
+		cwait(&not_empty);//CHECK??
 		sem_wait(&mutex);
 		int data = buffer[out];
 		sleep((double) data / 50); // Not required. Used for testing.
 		printf("%d\n", data);
-		sem_post(&mutex);
-		sem_post(&empty);
-		out = (out + 1) % BUFFER_SIZE;
-	}
+		cpost(&not_full);//CHECK?
+
+		//CHECK??
+		if (next_count > 0)
+			sem_post(&next);
+		else{
+			sem_post(&mutex);
+			out = (out + 1) % BUFFER_SIZE;
+		}	
 	pthread_exit(0);
+}
+
+// Semaphore implementation of conditional wait
+void cwait(struct condition *c) {
+	c->count++;
+	if (next_count > 0)
+		sem_post(&next);
+	else
+		sem_post(&mutex);
+	sem_wait(&(c->sem));
+	c->count--;
+}
+
+// Semaphore implementation of conditional signal
+void cpost(struct condition *c) {
+	if (c->count > 0) {
+		next_count++;
+		sem_post(&(c->sem));
+		sem_wait(&next);
+		next_count--;
+	}
 }
--- hw8/hw8_1.c
+++ hw8/hw8_1.c
@@ -10,32 +10,42 @@
 #define N 10
 #define BUFFER_SIZE 5
 
-/* Global variables are shared by the thread(s) */
-struct condition{
-	sem_t mutex;
-	sem_t not_full;
-	sem_t not_empty;
-	int count;
-}
+// Struct for condition variable
+struct condition {
+	sem_t sem; // Semaphore to be initialized to 0
+	int count; // Count of threads waiting
+};
 
-struct condition ac;
+//not empty count: number of producers waiting to produce
+//not full count: number of consumers waiting to consume
 
-int buffer[BUFFER_SIZE];
+struct condition not_full, not_empty;
+//call cpost not empty after adding an item
+//call cpost not full 
 
-void *child(); /* child thread */
 void cwait(struct condition *c); // Semaphore implementation of conditional wait
 void cpost(struct condition *c); // Semaphore implementation of conditional signal
 
+/* Global variables are shared by the thread(s) */
+sem_t mutex, next; //got rid of original 2nd and 3rd, added a next
+int buffer[BUFFER_SIZE];
+int next_count = 0;
+
+int buffer_space;
+
+void *child(); /* child thread */
+
 int main(int argc, char *argv[])
 {
-	//initialize all semaphores
-	if(sem_init(&(ac.mutex), 0, 1) < 0 || sem_init(&(ac.not_full), 0, 1) < 0 || sem_init(&(ac.not_empty), 0, 0) < 0) { // 0 = multithreaded
+	//init sems within the conditions
+	if(sem_init(&mutex, 0, 1) < 0 || sem_init(&next, 0, 0) < 0 || sem_init(&(not_full.sem), 0, 0) < 0 || sem_init(&(not_empty.sem), 0, 0) < 0) { // 0 = multithreaded
 		fprintf(stderr, "ERROR: could not initialize &semaphore.\n");
 		exit(0);
 	}
-	//initialize count field inside ac
-	ac.count = 0;
+	not_full.count = 0;
+	not_empty.count = 0;
 
+	buffer_space = BUFFER_SIZE;
 	// Prepare the child thread
 	pthread_t tid; /* the thread identifier */
 	pthread_attr_t attr; /* set of attributes for the thread */
@@ -46,41 +56,54 @@ int main(int argc, char *argv[])
 	int data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // Must have N values
 	int i, in = 0; // Index for writing
 	for(i = 0; i < N; i++) {
-
-		sem_wait(&(ac.not_full))
-		sem_wait(&(ac.mutex))
-
+		sem_wait(&mutex);
+		if(buffer_space == 0){
+			cwait(&not_full);
+		}
 		sleep((double) data[i] / 50); // Not required. Used for testing.
 		buffer[in] = data[i];
-		ac.count += 1;
+		buffer_space--;
+		cpost(&not_empty);//CHECK?
 
-		if(ac.count != BUFFER_SIZE){
-			sem_post(&(ac.not_full));
+
+		//CHECK??
+		if (next_count > 0)
+			sem_post(&next);
+		else{
+			sem_post(&mutex);
 		}
-
-		sem_post(&(ac.mutex));
-		sem_post(&(ac.not_empty));
-
 		in = (in + 1) % BUFFER_SIZE;
 	}
 	// Join and print result
 	pthread_join(tid, NULL);
+
 	sem_destroy(&mutex);
-	sem_destroy(&full);
-	sem_destroy(&empty);
+
+	//CHECK?? what to replace these with
+	//sem_destroy(&full);
+	//sem_destroy(&empty);
 }
 
 void *child() {
 	int i, out = 0; // Index for reading
 	for(i = 0; i < N; i++) {
-		sem_wait(&full);
 		sem_wait(&mutex);
+		if(buffer_space == BUFFER_SIZE){
+			cwait(&not_empty);
+		}
 		int data = buffer[out];
-		sleep((double) data / 50); // Not required. Used for testing.
+		//sleep((double) data / 50); // Not required. Used for testing.
 		printf("%d\n", data);
-		sem_post(&mutex);
-		sem_post(&empty);
-		out = (out + 1) % BUFFER_SIZE;
+		buffer_space++;
+		cpost(&not_full);//CHECK?
+
+		//CHECK??
+		if (next_count > 0)
+			sem_post(&next);
+		else{
+			sem_post(&mutex);
+			out = (out + 1) % BUFFER_SIZE;
+		}
 	}
 	pthread_exit(0);
 }
