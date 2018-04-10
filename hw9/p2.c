f8c386bae13a1b9fb518d242c8338d641c859cf9
kevin <kbradner1122gmail.com>
2018-04-10 12:23:19 -0400

    p2

--- hw9/p2.c
+++ hw9/p2.c
@@ -14,6 +14,8 @@
 /* Global variables are shared by the thread(s) */
 sem_t sem; // Semaphore
 double global_min = 0; // Global minimum
+double range;
+double stepSize = 0.005;
 
 double shubert(double step, double x2);
 void *child(); /* child thread */
@@ -26,51 +28,53 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	struct timeval start, end, diff;  // timers
-	gettimeofday(&start, NULL); // Unix timer
-	// Prepare the child thread
-	pthread_t tid; /* the thread identifier */
-	pthread_attr_t attr; /* set of attributes for the thread */
-	pthread_attr_init(&attr);
-	pthread_create(&tid, &attr, child, NULL);
+	int num_threads;
+	if (argc > 1)
+		num_threads = atoi(argv[1]);
+	else
+		num_threads = 5;
 
-	// Parent does work
-	double step = 0.005, x1_start, x1_stop, x1, x2, y;
-	x1_start = step; x1_stop = 2;
-	for (x1 = x1_start; x1 <= x1_stop; x1 += step) {
-		for (x2 = -2; x2 <= 2; x2 += step) {
-			y = shubert(x1, x2);
-			// *** ENTRY FOR CRITICAL SECTION ***
-			sem_wait(&sem);
-			// *** BEGIN CRITICAL SECTION ***
-			if (y < global_min)
-				global_min = y;
-			// *** EXIT FROM CRITICAL SECTION ***
-			sem_post(&sem);
-		}
+	struct timeval t_start, end, diff;  // timers
+	gettimeofday(&t_start, NULL); // Unix timer
+	// Prepare the child thread
+	pthread_t tid[num_threads]; /* the thread identifier */
+	double start[num_threads];
+	range = 4.0 / num_threads;
+
+	for (int i = 0; i < num_threads; i++) {
+		start[i] = i * range - 2;
+		pthread_create(&tid[i], NULL, child, &start[i]);
 	}
-	// Join and print result
-	pthread_join(tid, NULL);
+
+	for (int i = 0; i < num_threads; i++)
+		pthread_join(tid[i], NULL);
+	
 	gettimeofday(&end, NULL);
-	timersub(&end, &start, &diff);
+	timersub(&end, &t_start, &diff);
 	printf("min = %.2f, time = %.2f ", global_min, diff.tv_sec+diff.tv_usec/1000000.0);
 }
 
-void *child() {
-	double step = 0.5, x1_start, x1_stop, x1, x2, y;
-	x1_start = -2; x1_stop = 0;
-	for (x1 = x1_start; x1 <= x1_stop; x1 += step) {
-		for (x2 = -2; x2 <= 2; x2 += step) {
+void *child(void *start) {
+	double x1, x2, y;
+	double *x1_start = start;
+	double x1_stop = *x1_start + range - stepSize;
+	// printf("x1_start = %.2f, x1_start = %.2f\n", *x1_start, x1_stop);
+	// fflush(stdout);
+	double local_min = 0; // Local minimum for each thread
+	for (x1 = *x1_start; x1 <= x1_stop; x1 += stepSize) {
+		for (x2 = -2; x2 <= 2; x2 += stepSize) {
 			y = shubert(x1, x2);
-			// *** ENTRY FOR CRITICAL SECTION ***
-			sem_wait(&sem);
-			// *** BEGIN CRITICAL SECTION ***
-			if (y < global_min)
-				global_min = y;
-			// *** EXIT FROM CRITICAL SECTION ***
-			sem_post(&sem);
+			if (y < local_min)
+				local_min = y;
 		}
 	}
+	// *** ENTRY FOR CRITICAL SECTION ***
+	sem_wait(&sem);
+	// *** BEGIN CRITICAL SECTION ***
+	if (local_min < global_min)
+		global_min = local_min;
+	// *** EXIT FROM CRITICAL SECTION ***
+	sem_post(&sem);
 	pthread_exit(0);
 }
 
